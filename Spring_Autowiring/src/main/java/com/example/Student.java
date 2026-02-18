10668852d2a3b36cb44903fae128dfd1e652135a
saiaditya <2400030579@kluniversity.in>
2026-02-18 10:10:28 +0530

    Added Skill 5 - Spring Autowiring

---
+++ Spring_Autowiring/src/main/java/com/example/Student.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+
+    private int id = 101;
+    private String name = "Aditya";
+    private String gender = "Male";
+
+    @Autowired
+    private Certification certification;
+
+    public void display() {
+        System.out.println("Student ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Gender: " + gender);
+        certification.display();
+    }
+}
